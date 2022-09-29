94f6b058a3662f503f7db3a6f9217913671b7ad9
Bartlomiej Obudzinski <bobudzinski@amplifier.pl>
2022-09-29 19:45:38 +0200

    global consts

---
+++ src/Setup/theme.ts
@@ -0,0 +1,17 @@
+const theme: {
+  colors: { [key: string]: string };
+  fontSize: { [key: string]: number };
+} = {
+  colors: {
+    WHITE: '#FFF',
+    DARK_WHITE: '#eeeeee66',
+    DEEP_DARK_BLUE: '#00141e',
+  },
+  fontSize: {
+    small: 15,
+    medium: 20,
+    large: 25,
+  },
+};
+
+export default theme;
