80b9223c96ce111fc40a42388615ede2d05b5129
Bartlomiej Obudzinski <bobudzinski@amplifier.pl>
2022-10-04 17:34:56 +0200

    update paths in babel file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            Components: './src/Components',
+            Navigation: './src/Navigation',
+            Setup: './src/Setup',
+            Utils: './src/Utils',
+          },
+        },
+      ],
+    ],
   };
 };
